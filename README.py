d2e6322ee7f6ce8ab70a6ff8a3f51762522d516e
davew@microsoft.com <davew@microsoft.com>
2021-04-14 15:54:14 +0000

    init

---
+++ README.py
@@ -0,0 +1,5 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ## README:  databricks-workspace repo
+# MAGIC 
+# MAGIC This is my repo for databricks projects linked to my [Data Engineering Workshop](https://git.davewentzel.com/workshops/data-engineering).  
